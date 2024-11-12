bc0f4d33ba772382e492d1bb410e133caea2c421
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2024-11-12 21:22:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-WEATHER_API_KEY = #os.getenv('WEATHER_API_KEY')
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 UPLOAD_FOLDER = './uploads'
 
 # Creates the upload folder if it does not already exist
