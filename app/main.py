0a6782bfeb53dff2a90b3e7b07050aa45aa56c71
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2024-11-12 15:08:33 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-from app import create_app
-
-# Create the Flask app using the factory pattern
-app = create_app()
-
+from flask import Flask
+from routes import app
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
